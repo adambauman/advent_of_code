666d94ce44952e786b5f09f24d5eed9c3cd6692a
Adam J. Bauman <adam@kungfutreachery.net>
2020-12-05 23:54:17 -0600

    Refactored for Solution B

--- 2020/Day03_Pygame/Day03_Pygame.py
+++ 2020/Day03_Pygame/Day03_Pygame.py
@@ -3,7 +3,7 @@ from time import time_ns
 
 class Color:
     tree = (18, 150, 27)
-    snow = (255, 255, 255)
+    snow = (240, 240, 240)
     collision = (186, 9, 9)
 
 def load_data(file_path):
@@ -47,16 +47,17 @@ def draw_cell_line(line_data):
 
     return line_surface
 
-def draw_map(map_data):
+def draw_map(map_data, slope):
     assert 0 != len(map_data)
 
     print("Drawing the map...")
     
     # Moving 1 line down for every 3 columns right, create the base surface
-    map_size = (len(map_data) * 3, len(map_data))
+    map_size = (len(map_data) * slope[0], len(map_data))
     map_surface = pygame.Surface(map_size)
 
-    print("Map will be {} by {} pixels".format(map_size[0], map_size[1]))
+    if __debug__:
+        print("Map will be {} by {} pixels".format(map_size[0], map_size[1]))
     
     # Create our map cell, this will repeat across the width of the map surface
     cell_size = (len(map_data[0]), len(map_data))
@@ -66,7 +67,7 @@ def draw_map(map_data):
         cell_surface.blit(draw_cell_line(line), (0, current_cell_y))
         current_cell_y += 1
 
-    #pygame.image.save(cell_surface, 'cell_output.png
+    #pygame.image.save(cell_surface, "cell_output.png")
 
     # Blit down the map cell until we fill the whole map
     current_map_x = 0
@@ -78,34 +79,61 @@ def draw_map(map_data):
 
     return map_surface
 
-def solve_a(map_surface):
-    collision_map = map_surface.copy()
-    collision_count = 0
+def solve_for_slope(map_data, slope):
+    assert 0 != len(map_data)
 
-    px_array = pygame.PixelArray(collision_map)
+    map_surface = draw_map(map_data, slope)
+    collision_count = 0
+    px_array = pygame.PixelArray(map_surface)
     cursor_x = 0
     cursor_y = 0
-    while cursor_x < collision_map.get_width():
-        if collision_map.map_rgb(Color.tree) == px_array[cursor_x, cursor_y]:
+    while cursor_x < map_surface.get_width() and cursor_y < map_surface.get_width():
+        if map_surface.map_rgb(Color.tree) == px_array[cursor_x, cursor_y]:
             collision_count += 1
             px_array[cursor_x, cursor_y] = Color.collision
      
-        cursor_x += 3
-        cursor_y += 1
+        cursor_x += slope[0]
+        cursor_y += slope[1]
 
-    return collision_map, collision_count
+    px_array.close()
+    print("Saving map PNG...")
+    map_name = "map_{}x{}.png".format(slope[0], slope[1])
+    pygame.image.save(map_surface, map_name)
+
+    return collision_count
+
+
+def solve_for_multi_slope(map_data, slopes):
+    assert 0 != len(slopes)
+    assert 0 != len(map_data)
+
+    # Right 1, down 1.
+    # Right 3, down 1. (This is the slope you already checked.)
+    # Right 5, down 1.
+    # Right 7, down 1.
+    # Right 1, down 2.
+    # Multiply all collision counts, that is the answer
+
+    print("Starting solve for Solution B...")
+
+    solution_answer = 0
+    for slope in slopes:
+        collision_count = solve_for_slope(map_data, slope)
+        if 0 == solution_answer:
+            solution_answer = collision_count
+            continue
+
+        if __debug__:
+            print("Current solution answer: {}".format(solution_answer))
+            print("Collision count: {}".format(collision_count))
+
+        solution_answer *= collision_count
+
+    return solution_answer
 
 
 def main():
     pygame.init()
-    pygame.mixer.quit()
-    pygame.mouse.set_visible(False)
-    pygame.event.set_allowed([pygame.QUIT])
-
-    #display_surface = pygame.display.set_mode(
-    #    (Hardware.screen_width, Hardware.screen_height),
-    #    pygame.HWSURFACE | pygame.DOUBLEBUF
-    #)
 
     print("")
     print("+--------------------------------------------+")
@@ -113,30 +141,26 @@ def main():
     print("+--------------------------------------------+")
     print("")
     print("Solution A: Using forest map determine toboggan collisions moving right 3, down 1")
-    print("Solution B: ???")
+    print("Solution B: Same thing but with variable slopes")
     print("")
 
     file_path = "data.txt"
     map_data = load_data(file_path)
     assert(0 != len(map_data))
 
-    map_surface = draw_map(map_data)
-    collision_map, collision_count = solve_a(map_surface)
-
+    # Solution A: 3 right, 1 down
+    collision_count = solve_for_slope(map_data, (3,1))
     print("========================================")
     print("Solution A: {} collisions".format(collision_count))
     print("========================================")
-    print("Saving collision map to solution_a_map.png")
-    pygame.image.save(collision_map, "./solution_a_map.png")
 
-
-        # for event in pygame.event.get():
-        #    if event.type == pygame.QUIT:
-        #        print("User quit")
-        #        pygame.quit()
-        #        sys.exit()
-        #pygame.event.clear()
-
+    # Solution B: Variable, multiple all collision counts for answer
+    print("")
+    slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+    solution_b_answer = solve_for_multi_slope(map_data, slopes)
+    print("========================================")
+    print("Solution B: {}".format(solution_b_answer))
+    print("========================================")
 
     pygame.quit()
 
