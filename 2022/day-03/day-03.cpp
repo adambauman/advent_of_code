a4c662334f3bce2b062e730f61025c5c5e121808
Adam J. Bauman <adam@kungfutreachery.net>
2022-12-04 10:27:01 -0600

    Part two solve.

--- 2022/day-03/day-03.cpp
+++ 2022/day-03/day-03.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <cassert>
+#include <vector>
 
 #ifdef _DEBUG
 constexpr auto debug{ true };
@@ -23,29 +24,6 @@ namespace {
         std::ifstream file;
     };
 
-    char get_common_item(const std::string& sack_contents) {
-    
-        assert(!sack_contents.empty());
-     
-        if (0 != sack_contents.size() % 2) { throw(std::exception("Check if contents is divisible by 2")); }
-
-        const auto compartment_size{ sack_contents.size() / 2 };
-        const auto& first_compartment{ sack_contents.substr(0, compartment_size)};
-        const auto& second_compartment{ sack_contents.substr(compartment_size) };
-        assert(first_compartment.size() == second_compartment.size());
-
-        char common_item{ 0 };
-        for (const auto item : first_compartment) {
-
-            if (second_compartment.npos == second_compartment.find(item)) { continue; }
-
-            common_item = item; 
-            break;
-        }
-
-        return(common_item);
-    }
-
     int inline get_priority(const char item) {
 
         assert(0 != item);
@@ -59,6 +37,78 @@ namespace {
     }
 }
 
+namespace SingleSack {
+    void get_common_items(std::string& common_items, const std::string& this_sack, const std::string& that_sack) {
+
+        assert(!this_sack.empty());
+        assert(!that_sack.empty());
+
+        for (const auto item : this_sack) {
+            if (that_sack.npos == that_sack.find(item)) { continue; }
+
+            if (common_items.npos == common_items.find(item) || common_items.empty()) {
+                common_items.push_back(item);
+            }
+        }
+    }
+    char get_common_item(const std::string& sack) {
+
+        assert(!sack.empty());
+
+        if (0 != sack.size() % 2) { throw(std::exception("Check if contents is divisible by 2")); }
+
+        const auto compartment_size{ sack.size() / 2 };
+        const auto& first_compartment{ sack.substr(0, compartment_size) };
+        const auto& second_compartment{ sack.substr(compartment_size) };
+        assert(first_compartment.size() == second_compartment.size());
+
+        char common_item{ '\0' };
+        for (const auto item : first_compartment) {
+            if (second_compartment.npos == second_compartment.find(item)) { continue; }
+
+            common_item = item;
+            break;
+        }
+
+        return(common_item);
+    }
+}
+namespace GroupSacks {
+
+    bool item_exists_in_all_sacks(std::vector<std::string>&sacks, const char item) {
+
+        assert(!sacks.empty());
+        assert('\0' != item);
+
+        auto exists_in_all_sacks{ true };
+        for (const auto& sack : sacks) {
+
+            if (sack.npos == sack.find(item)) { 
+                exists_in_all_sacks = false;
+                break;            
+            }
+        }
+        return(exists_in_all_sacks);
+    }
+    char find_badge(std::vector<std::string>& sacks) {
+         
+        assert(!sacks.empty());
+
+        const auto& first_sack{ sacks.front() };
+        char badge_item{ '\0' };
+        for (const auto item : first_sack) {
+
+            auto exists_in_all_sacks{ item_exists_in_all_sacks(sacks, item) };
+            if (!exists_in_all_sacks) { continue; }
+
+            badge_item = item;
+            break;
+        }
+
+        return(badge_item);
+    }
+}
+
 constexpr auto input_path{ ".\\input.txt" };
 int main() try {
 
@@ -67,15 +117,27 @@ int main() try {
     std::cout << "Checking contents of rucksacks...\n";
 
     auto common_item_priority_total{ 0 };
+    auto badges_priority_total{ 0 };
+    std::vector<std::string> group_sacks;
+    constexpr auto group_size{ 3 };
     for (std::string line; std::getline(input_file.file, line);) {
 
-        const auto common_item{ get_common_item(line) };
+        const auto common_item{ SingleSack::get_common_item(line) };
         common_item_priority_total += get_priority(common_item);
 
-        //if constexpr (debug) { std::cout << line << ": " << common_item << " priority: " << std::dec << item_priority << "\n"; }
+        group_sacks.push_back(line);
+        if (group_size == group_sacks.size()) {
+
+            const auto badge_item{ GroupSacks::find_badge(group_sacks) };
+            badges_priority_total += get_priority(badge_item);
+            group_sacks.clear();
+        }
     }
 
-    std::cout << "Total from common item priorities: " << std::dec << common_item_priority_total << "\n";
+    std::cout
+        << "Total from common item priorities: " << std::dec << common_item_priority_total << "\n"
+        << "Total from  badge item priorities: " << std::dec << badges_priority_total << "\n";
+
 
     return(EXIT_SUCCESS);
 
